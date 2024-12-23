16b82663a3903ecf0ee6240bd7351233164536c5
Charlélie CARER <charlelie.carer@gmail.com>
2024-12-23 21:17:56 +0100

    Working version for Chabert

---
+++ run.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.constants import k, e, pi
+from model import GlobalModel
+from config import config_dict
+
+print("start")
+
+if __name__ == "__main__":
+    model = GlobalModel(config_dict)
+
+    # Solve for several values of I_coil
+
+    I_coil = np.linspace(1, 40, 20)
+    p, s = model.solve_for_I_coil(I_coil)
+
+    T_e = s[:, 0]
+    T_g = s[:, 1]
+    n_e = s[:, 2]
+    n_g = s[:, 3]
+
+    print("plot start")
+
+    thrust = model.eval_property(model.thrust_i, s)
+    j_i = model.eval_property(model.j_i, s)
+    plt.ylim(0, 200)
+    plt.xlim(0, 1600)
+    plt.plot(p, j_i)
+    plt.title("Current density as function of power in the coil")
+    plt.show(block=False)
+    print("thruster plot")
+
+    #exit()
+    # Temperature plot
+
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+    ax2.plot(p, T_e * k / e, 'g-')
+    ax1.plot(p, T_g, 'b-')
+
+    ax1.set_xlabel('$P_{RF}$ [W]')
+    ax2.set_ylabel('$T_e$ [eV]', color='g')
+    ax1.set_ylabel('$T_g$ [K]', color='b')
+
+    ax1.set_xlim((0, 1600))
+    ax2.set_ylim(((T_e * k / e).min(), 5.3))
+    ax1.set_ylim((255, 540))
+
+    plt.title("Gas and electron temperature as function of power")
+
+    plt.show(block=False)
+    print("2nd plot")
+
+    # Density plot
+    plt.xlim((0, 1600))
+    plt.semilogy(p, n_e, label='$n_e$')
+    plt.semilogy(p, n_g, label='$n_g$')
+    plt.xlabel('n [$m^{-3}$]')
+    plt.xlabel('$P_{RF}$ [W]')
+    plt.legend()
+    plt.title("Gas and electron (aka ion) density as function of power")
+    plt.show()
+    print("desity plot")
+
---
+++ util.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def load_csv(filename, sep=';', skiprows=0):
+    return np.loadtxt(open(filename, "rb"), delimiter=sep, skiprows=skiprows)
+
+def load_cross_section(filename):
+    """Takes a text file with two columns of numbers and return two arrays : one for each column"""
+    data_cs = load_csv(filename)
+    return data_cs[:,0], data_cs[:,1]
