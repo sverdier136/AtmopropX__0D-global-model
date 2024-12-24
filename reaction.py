5539fd79ccd51d743cf2653d96d4a2e1d72f02ba
Charlélie CARER <charlelie.carer@gmail.com>
2024-12-24 02:40:47 +0100

    Reaction+Species working

--- reaction.py
+++ reaction.py
@@ -1,27 +1,59 @@
 import numpy as np
+from specie import Specie, Species
 
-
-species_list = ["I2", "I1/3", "I+"]
 
 class Reaction:
 
-    def __init__(self, species_list, reactives, products, rate_constant, energy_treshold, stoechio_coeffs=None):
-        """Init of Reaction class.
+    def __init__(self, species, reactives, products, rate_constant, energy_treshold, stoechio_coeffs=None):
+        """
+        Reaction class
             Inputs : 
-            species_list : list of all species present (instances of class Specie)
-            reactives : list with all reactives
-            products : list with all products
-            rate_constant : function taking as arguments (T_g, T_e, )
-            stoechio_coeffs : stoechiometric coefficients positive if product, negative otherwise"""
-        self.species = species_list
+                species : instance of class Species, lists all species present 
+                reactives : list with all reactives names
+                products : list with all products names
+                rate_constant : function taking as arguments (T_g, T_e, )
+                stoechio_coeffs : stoechiometric coefficients positive if product, negative otherwise"""
+        self.species = species
+
+        self.reactives = [self.species.get_specie_by_name(name) for name in reactives]
+        self.reactives_indices = [self.species.get_index_by_instance(sp) for sp in self.reactives]
+
+        self.products = [self.species.get_specie_by_name(name) for name in products]
+        self.products_indices = [self.species.get_index_by_instance(sp) for sp in self.products]
+
         if stoechio_coeffs :
             self.stoechio_coeffs = np.array(stoechio_coeffs)
         else:
-            self.stoechio_coeffs = np.ones(len(self.species))
+            # sets stoechio_coeffs at 1 for reactives and products if not defined
+            self.stoechio_coeffs = np.zeros(self.species.nb)
+            for i in self.reactives_indices:
+                self.stoechio_coeffs[i] = 1
+            for sp in self.products_indices:
+                self.stoechio_coeffs[i] = 1
+
         self.energy_threshold = energy_treshold
         self.rate_constant = rate_constant     # func
-        self.
+        #self.
 
     def change_rate(self, T_g, T_e, densities):
         """Returns an np.array with the change rate for each species due to this reaction"""
-        return 
+        K = self.rate_constant(T_g, T_e)
+        product = K * np.prod(densities[self.reactives_indices+self.products_indices]) # product of rate constant and densities of all the stuff
+        rate = np.zeros(self.species.nb)
+        for sp in self.reactives:
+            rate[self.species.get_index_by_instance(sp)] = - product
+        for sp in self.products:
+            rate[self.species.get_index_by_instance(sp)] = + product
+        
+        return rate
+
+
+if __name__ == "__main__":
+    def K(T_g, T_e):
+        return 2
+    
+    species_list = Species([Specie("I0", 10.57e-27, 0), Specie("I1", 10.57e-27, 0), Specie("I2", 10.57e-27, 0), Specie("I3", 10.57e-27, 0), Specie("I4", 10.57e-27, 0), Specie("I5", 10.57e-27, 0)])
+
+    reac = Reaction(species_list, ["I2", "I4"], ["I5"], K, 10)
+    densities = np.array([1,2,3,4,5,6])
+    print(reac.change_rate(1,1,densities))
--- specie.py
+++ specie.py
@@ -3,9 +3,46 @@
 class Specie:
 
     species = []
-    species_name
+    species_name = []
 
     def __init__(self, name, mass, charge):
         self.name = name
         self.mass = mass
         self.charge = charge
+        self.index = None
+
+class Species:
+
+    def __init__(self, species_list):
+        self.species = species_list
+        self.names = [sp.name for sp in self.species]
+
+        for i, sp in enumerate(self.species):
+            sp.index = i
+
+    @property
+    def nb(self):
+        return len(self.species)
+
+    def add(self, specie):
+        self.species.append(specie)
+
+    def get_index_by_name(self, name):
+        for i, sp in enumerate(self.species):
+            if sp.name == name:
+                return i
+        raise Exception(f"There is no {name} in this species group")
+    
+    def get_index_by_instance(self, inst):
+        for i, sp in enumerate(self.species):
+            if sp is inst:
+                return i
+        raise Exception(f"There is no {inst} in this species group")
+    
+    def get_specie_by_name(self, name):
+        for sp in self.species:
+            if sp.name == name:
+                return sp
+        raise Exception(f"There is no {name} in this species group")
+
+# species_list = Species([Specie("I0", 10.57e-27, 0), Specie("I1", 10.57e-27, 0), Specie("I2", 10.57e-27, 0), Specie("I3", 10.57e-27, 0), Specie("I4", 10.57e-27, 0), Specie("I5", 10.57e-27, 0)])
