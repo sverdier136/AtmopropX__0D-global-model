5c00b6b7051bb13764c30b5b5b82c7b91fa285d2
Charlélie CARER <charlelie.carer@gmail.com>
2025-02-05 18:42:49 +0100

    EllasticCollisionElectron ready with diato/mono temps

--- src/reactions/ellastic_collision_with_electrons_reaction.py
+++ src/reactions/ellastic_collision_with_electrons_reaction.py
@@ -8,21 +8,41 @@ from src.specie import Specie, Species
 from src.reactions.reaction import Reaction
 
 class ElasticCollisionWithElectron(Reaction):
+    """
+    Elastic collision between a particle and an electron
+    Works with 3 temperatures : Te, Tmono, Tdiat
+    In reactives, electron must be in first position and colliding_specie next.
+    """
 
     def __init__(self, species: Species, colliding_specie: str, rate_constant, energy_treshold: float):
-        super().__init__(species, [colliding_specie], [colliding_specie], rate_constant, energy_treshold)
+        """
+        Dissociation class
+        /!\ Electrons should NOT be added to reactives and products
+            Inputs : 
+                species : instance of class Species, lists all species present 
+                colliding_specie : name of specie that collides with an electron. Must be a string !
+                rate_constant : function taking as argument state [n_e, n_N2, ..., n_N+, T_e, T_monoato, ..., T_diato]
+                energy_threshold : energy threshold of electron so that reaction occurs
+        """
+        super().__init__(species, [species.names[0], colliding_specie], [species.names[0], colliding_specie], rate_constant, energy_treshold)
 
     @override
     def density_change_rate(self, state):
         return np.zeros(self.species.nb)
 
-
+    
     @override
-    def electron_loss_power(self, state):
-        K = self.rate_constant(state)
-        mass_ratio = m_e / self.reactives[0].mass
-        delta_temp = state[self.species.nb] - state[self.species.nb + self.reactives[0].nb_atoms ]
+    def energy_change_rate(self, state):
+        rate = np.zeros(3)
 
-        energy_change = 3 * mass_ratio * k_B * delta_temp * state[0] * state[self.reactives_indices[0]] * K 
-   
-        return energy_change
+        K = self.rate_constant(state)
+        reac_speed = K * np.prod(state[self.reactives_indices])
+        mass_ratio = m_e / self.reactives[1].mass  # self.reactives[1].mass is mass of colliding_specie
+        delta_temp = state[self.species.nb] - state[self.species.nb + self.reactives[1].nb_atoms]  # Te - Tspecie
+
+        energy_change = 3 * mass_ratio * k_B * delta_temp * reac_speed 
+        
+        rate[0] = -energy_change
+        rate[self.reactives[1].nb_atoms] = energy_change #mono / diatomic particles gain energy, electrons lose energy
+
+        return rate
