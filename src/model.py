301e7c711f22c8fdfce67d32fc817ce5fd59a99b
Charlélie CARER <charlelie.carer@gmail.com>
2025-02-12 16:32:05 +0100

    f_dy ready for Reaction

--- src/model.py
+++ src/model.py
@@ -9,67 +9,52 @@ from src.util import load_csv, load_cross_section
 from src.auxiliary_funcs import pressure, maxwellian_flux_speed, u_B, A_eff, A_eff_1, SIGMA_I, R_ind, h_L
 from src.specie import Specie, Species
 from reactions.reaction import Reaction
+from config import *
 
 class GlobalModel:
 
-    def __init__(self, config_dict: dict[str, float], species: Species, reaction_set: list[Reaction]):
+    def __init__(self, species: Species, reaction_set: list[Reaction]):
         """Object simulating the evolution of a plasma with 0D model. 
             Inputs :
                 config_dict : dictionary containing all parameters about the experimental setup
                 species : instance of Species with all species being considered
                 reaction_set : list with all reactions being considered -> [Reaction]"""
         self.load_chemistry()
-        self.load_config(config_dict)
         self.species = species
         self.reaction_set = reaction_set
 
-    def load_config(self, config_dict: dict[str, float]):
+    # def load_config(self, config_dict: dict[str, float]):
 
-        # Geometry
-        self.R = config_dict['R'] # Chamber's radius 
-        self.L = config_dict['L'] # Length
+    #     # Geometry
+    #     self.R = config_dict['R'] # Chamber's radius 
+    #     self.L = config_dict['L'] # Length
         
-        # Neutral flow
-        self.m_i = config_dict['m_i']
-        self.Q_g = config_dict['Q_g']
-        self.beta_g = config_dict['beta_g']
-        self.kappa = config_dict['kappa']
+    #     # Neutral flow
+    #     self.m_i = config_dict['m_i']
+    #     self.Q_g = config_dict['Q_g']
+    #     self.beta_g = config_dict['beta_g']
+    #     self.kappa = config_dict['kappa']
 
-        # Ions
-        self.beta_i = config_dict['beta_i'] # Transparency to ions (!!! no exact formula => to be determined later on...)
-        self.V_grid = config_dict['V_grid'] # potential difference
+    #     # Ions
+    #     self.beta_i = config_dict['beta_i'] # Transparency to ions (!!! no exact formula => to be determined later on...)
+    #     self.V_grid = config_dict['V_grid'] # potential difference
 
-        # Electrical
-        self.omega = config_dict['omega']  # Pulsation of electro-mag fiel ?
-        self.N = config_dict['N']  # Number of spires
-        self.R_coil = config_dict['R_coil'] #Radius of coil around plasma
-        self.I_coil = config_dict['I_coil'] 
+    #     # Electrical
+    #     self.omega = config_dict['omega']  # Pulsation of electro-mag fiel ?
+    #     self.N = config_dict['N']  # Number of spires
+    #     self.R_coil = config_dict['R_coil'] #Radius of coil around plasma
+    #     self.I_coil = config_dict['I_coil'] 
 
-        # Initial values = values in steady state before ignition of plasma
-        self.T_e_0 = config_dict['T_e_0']
-        self.n_e_0 = config_dict['n_e_0']
-        self.T_g_0 = config_dict['T_g_0']
-        # gas density (in m^-3 ??) in steady state without plasma
-        self.n_g_0 = pressure(self.T_g_0, self.Q_g,     
-                              maxwellian_flux_speed(self.T_g_0, self.m_i),
-                              self.A_g) / (k * self.T_g_0)
+    #     # Initial values = values in steady state before ignition of plasma
+    #     self.T_e_0 = config_dict['T_e_0']
+    #     self.n_e_0 = config_dict['n_e_0']
+    #     self.T_g_0 = config_dict['T_g_0']
+    # # gas density (in m^-3 ??) in steady state without plasma
+    #       self.n_g_0 = pressure(self.T_g_0, self.Q_g,     
+    #                               maxwellian_flux_speed(self.T_g_0, self.m_i),
+    #                               self.A_g) / (k * self.T_g_0)
     
-    @property
-    def A_g(self): return self.beta_g * pi * self.R**2
-
-    @property
-    def A_i(self): return self.beta_i * pi * self.R**2
-
-    @property
-    def V(self): return pi * self.R**2 * self.L 
-
-    @property
-    def A(self): return 2*pi*self.R**2 + 2*pi*self.R*self.L
-
-    @property
-    def v_beam(self): 
-        """Ion beam's exit speed"""
-        return np.sqrt(2 * e * self.V_grid / self.m_i)
+    
 
     def flux_i(self, T_e, T_g, n_e, n_g):
         """Ion flux leaving the thruster through the grid holes"""
@@ -90,40 +75,12 @@ class GlobalModel:
         for i in np.arange(sol.shape[0]):
             prop[i] = func(sol[i])
         return prop
-
-    # def P_loss(self, state):
-    #     """
-    #     Calcule les pertes d'énergie des électrons :
-    #     - Ionisation
-    #     - Excitation
-    #     - Collisions élastiques
-    #     - Pertes aux parois
-    #     - Dissociation et excitation vibrationnelle (plasma d'air)
-    #     """
-    #     # Ionisation et excitation
-    #     a = self.E_iz * n_e * n_g * self.K_iz(T_e)
-    #     b = self.E_ex * n_e * n_g * self.K_ex(T_e)
-        
-    #     # Collisions élastiques (énergie transférée au gaz)
-    #     c = 3 * (m_e / self.m_i) * k * (T_e - T_g) * n_e * n_g * self.K_el(T_e)
-
-    #     # Pertes aux parois
-    #     d = 7 * k * T_e * n_e * u_B(T_e, self.m_i) * A_eff(n_g, self.R, self.L) / self.V
-
-    #     # Dissociation et excitation vibrationnelle 
-    #     K_diss = self.K_diss(T_e)  # Taux de dissociation
-
-    #     e = self.E_diss * n_e * n_g * K_diss
-    #     f = E_vibr * n_e * n_g * K_vibr
-
-    #     return a + b + c + d + e + f
-
-
     
 
     def P_abs(self, state):
         # ! n_g à changer
         return R_ind(self.R, self.L, self.N, self.omega, n_e, n_g, self.K_el(T_e)) * self.I_coil**2 / 2 # the original code divided by V : density of power ?
+    
     
     def electron_energy_derivative(self, state):
         """Returns the derivative of electron energy : d/dt (3/2 n_e e T_e)
@@ -137,42 +94,6 @@ class GlobalModel:
         power_balance = self.P_abs(state) - p_loss
 
         return power_balance
-
-
-    def monoatomic_gas_energy_derivative(self, state):
-        """
-        Calcule la dérivée de l'énergie du gaz : (3/2) * n_g * k_B * T_g
-        
-        Input :
-        'state' has format [n_e, n_N2, ..., n_N+, T_e, T_monoato, ..., T_diato]
-        """
-
-        # Taux de réaction
-        K_diss = self.K_diss(T_e)  # Taux de dissociation
-        K_vibr = self.K_vibr(T_e)  # Taux d'excitation vibrationnelle
-        K_rot = self.K_rot(T_e)  # Taux d'excitation rotationnelle
-
-        # Collisions élastiques : transfert d'énergie des électrons vers le gaz
-        a = 3 * (m_e / self.m_i) * k * (T_e - T_g) * n_e * n_g * self.K_el(T_e)
-
-        # Transfert d'énergie des ions au gaz neutre via collisions
-        b = (1/4) * self.m_i * (u_B(T_e, self.m_i)**2) * n_e * n_g * SIGMA_I * maxwellian_flux_speed(T_g, self.m_i)
-
-        # Dissociation des molécules 
-        c = E_diss * n_e * n_g * K_diss
-
-        # Excitation vibrationnelle 
-        d = E_vibr * n_e * n_g * K_vibr
-
-        # Excitation rotationnelle 
-        e = E_rot * n_e * n_g * K_rot
-
-        # Transfert de chaleur aux parois : calcul de lamda0 ? 
-        lambda_0 = self.R / 2.405 + self.L / pi  # Longueur de diffusion thermique
-        f = self.kappa * (T_g - self.T_g_0) * self.A / (self.V * lambda_0)
-
-        # Somme des contributions
-        return a + b + c + d + e - f
 
 
     def particles_densities_derivative(self, state: NDArray[float]): # type: ignore
@@ -203,14 +124,23 @@ class GlobalModel:
         return (1/2) * (R_ind_val + self.R_coil) * self.I_coil**2
 
     def f_dy(self, t, state):
-        # ! Dérivée en température !!!
         """Returns the derivative of the vector 'state' describing the state of plasma.
             'state' has format : [n_e, n_N2, ..., n_N+, T_e, T_monoato, ..., T_diato]"""
-        dy = np.zeros(state.shape)
+        densities = state[:self.species.nb]
+        temp_by_sp = [state[self.species.nb + sp.nb_atoms] for sp in self.species.list]
 
-        dy[:self.species.nb] = self.particles_densities_derivative(state)
-        dy[self.species.nb] = self.electron_energy_derivative(state)
-        dy[self.species.nb] = self.monoatomic_gas_energy_derivative(state)
+        dy = np.zeros(state.shape)
+        dy_densities = np.zeros(self.species.nb)
+        dy_energies = np.zeros(3)
+
+        for reac in self.reaction_set:
+            dy_densities += reac.density_change_rate(state)
+            dy_energies += reac.energy_change_rate(state)
+        #Transform derivative of energy into derivative of temperature
+        dy_temp = 2/3 * dy_energies/(e*densities) - dy_densities*temp_by_sp/densities
+
+        dy[:self.species.nb] = dy_densities
+        dy[self.species.nb:] = dy_temp
 
         return dy
     
@@ -239,5 +169,3 @@ class GlobalModel:
             solution[i] = final_state
             
         return p, solution
-
-#Test
