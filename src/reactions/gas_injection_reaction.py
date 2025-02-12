29d78db36d398b03336f2fe90128a3ab82ac0924
Charlélie CARER <charlelie.carer@gmail.com>
2025-02-12 15:20:11 +0100

    gas injection reaction done

--- src/reactions/gas_injection_reaction.py
+++ src/reactions/gas_injection_reaction.py
@@ -4,43 +4,38 @@ from numpy.typing import NDArray
 from scipy.constants import m_e, e, pi, k as k_B, epsilon_0 as eps_0, mu_0   # k is k_B -> Boltzmann constant
 
 from src.specie import Specie, Species
-
 from src.reactions.reaction import Reaction
+from src.config import *
 
 class GasInjection(Reaction):
     """
     Represents the gas injection : adds quantity to ne, ...
     """
 
-    def __init__(self, species: Species, injection_rates: NDArray[np.float]):
+    def __init__(self, species: Species, injection_rates: NDArray[np.float], T_injection: float):
         """
         Dissociation class
             Inputs : 
                 species : instance of class Species, lists all species present 
                 injection_rate : np.array with number of particle injected per second for each specie
                     Must have same dimesnsions and be in the same order as species.names
+                T_injection : temperature at which the gas is injected into the chamber
         """
         species_injected_names = [species.names[i] for i in range(species.nb) if injection_rates[i] != 0]
         super().__init__(species, [], species_injected_names)
         self.injection_rates = injection_rates
+        self.T_injection = T_injection
 
     @override
     def density_change_rate(self, state):
-        return self.injection_rates/
+        return self.injection_rates / V_chamber 
 
     
     @override
     def energy_change_rate(self, state):
         rate = np.zeros(3)
 
-        K = self.rate_constant(state)
-        reac_speed = K * np.prod(state[self.reactives_indices])
-        mass_ratio = m_e / self.reactives[1].mass  # self.reactives[1].mass is mass of colliding_specie
-        delta_temp = state[self.species.nb] - state[self.species.nb + self.reactives[1].nb_atoms]  # Te - Tspecie
-
-        energy_change = 3 * mass_ratio * k_B * delta_temp * reac_speed 
-        
-        rate[0] = -energy_change
-        rate[self.reactives[1].nb_atoms] = energy_change #mono / diatomic particles gain energy, electrons lose energy
+        for sp in self.products:
+            rate[sp.nb_atoms] += 3/2 * self.injection_rates[sp.index] / V_chamber * self.T_injection
 
         return rate
