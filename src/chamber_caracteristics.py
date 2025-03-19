f0c7de03dfe913263deb473b27912599a328ad81
adrienscemama <adriscemama23@free.fr>
2025-03-19 14:54:23 +0100

    Update chamber_caracteristics.py

--- src/chamber_caracteristics.py
+++ src/chamber_caracteristics.py
@@ -75,32 +75,7 @@ class Chamber(object):
     def gamma_ion(self, n_ion, T_e):
         #m_ion needs to be defined
         return self.h_L(n_ion, L) * n_ion * self.u_B(T_e, m_ion)
-iefhaoezfhazeidhfa
-    def thrust_i(self, T_e, n_e, n_ion):
-        """Thrust produced by the ion beam of one specie"""
-        return self.gamma_ion(n_ion, T_e, L) * self.m_i * self.v_beam * self.A_i
 
-    def j_i(self, T_e, n_e, n_ion):
-        """Ion current density of one ionic specie extracted by the grids"""
-        #it should take into account the +2 species of ions
-        return self.gamma_ion( n_ion, T_e, L) * e
-        
-    def total_ion_thrust(self , state ) :
-        '''Calculates the total amount of thrust generated'''
-        total_thrust = 0
-        for i in(range(1,len(state)/2)):
-            if species[i].charge != 0 :
-                total_thrust += self.thrust_i( state[len(state)/2] , state[0] , state[i])
-        return total_thrust
-
-    def total_ion_current(self , state , species) :
-        '''Calculates the total amount of ion current toxards the grids'''
-        total_current = 0
-        for i in(range(1,len(state)/2)):
-            if species[i].charge != 0 :
-                total_current += self.j_i( state[len(state)/2] , state[0] , state[i])
-        return total_current
-zieruhfaozuqegrfua
     def gamma_e(self, n_e, T_e):
         return 0.25*n_e * self.maxwellian_flux_speed(T_e, m_e)
 
