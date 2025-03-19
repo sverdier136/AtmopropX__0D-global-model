4881935f2af9d964137bc0b76931c2b6002e94a3
adrienscemama <adriscemama23@free.fr>
2025-03-19 14:39:42 +0100

    Update chamber_caracteristics.py

--- src/chamber_caracteristics.py
+++ src/chamber_caracteristics.py
@@ -36,10 +36,10 @@ class Chamber(object):
         self.n_e_0  = config_dict["n_e_0"]
         self.T_g_0  = config_dict["T_g_0"]
 
-
+    
     def u_B(self,T_e, m_ion):
         """T_e in eV, m_ion is mass of single ion"""
-        return np.sqrt(e*T_e/(N_A*m_ion))
+        return np.sqrt(k*T_e/(N_A*m_ion))
 
     def h_L(self, n_g, L):
         lambda_i = 1/(n_g * self.SIGMA_I)
@@ -50,7 +50,6 @@ class Chamber(object):
         return 0.8 / np.sqrt(4 + (R / lambda_i))
 
     def maxwellian_flux_speed(self, T, m):
-        # TODO : what is it, where does it come from, does it always hold true
         return np.sqrt((8 * k * T) / (pi * m))
 
     def pressure(self, T, Q, v, A_out):
@@ -73,10 +72,10 @@ class Chamber(object):
         nu_m_i = 1j * K_el * n_g
         return 1 - (omega_pe_sq / (omega * (omega -  nu_m_i)))
 
-    def gamma_ion(self, n_ion, T_e, L):
+    def gamma_ion(self, n_ion, T_e):
         #m_ion needs to be defined
         return self.h_L(n_ion, L) * n_ion * self.u_B(T_e, m_ion)
-
+iefhaoezfhazeidhfa
     def thrust_i(self, T_e, n_e, n_ion):
         """Thrust produced by the ion beam of one specie"""
         return self.gamma_ion(n_ion, T_e, L) * self.m_i * self.v_beam * self.A_i
@@ -90,20 +89,20 @@ class Chamber(object):
         '''Calculates the total amount of thrust generated'''
         total_thrust = 0
         for i in(range(1,len(state)/2)):
-            #test if it is an ion
-            total_thrust += self.thrust_i( state[len(state)/2] , state[0] , state[i])
+            if species[i].charge != 0 :
+                total_thrust += self.thrust_i( state[len(state)/2] , state[0] , state[i])
         return total_thrust
 
-    def total_ion_current(self , state ) :
+    def total_ion_current(self , state , species) :
         '''Calculates the total amount of ion current toxards the grids'''
         total_current = 0
         for i in(range(1,len(state)/2)):
-            #test if it is an ion
-            total_current += self.j_i( state[len(state)/2] , state[0] , state[i])
+            if species[i].charge != 0 :
+                total_current += self.j_i( state[len(state)/2] , state[0] , state[i])
         return total_current
-        
+zieruhfaozuqegrfua
     def gamma_e(self, n_e, T_e):
-        return n_e * self.u_B(T_e, m_e)
+        return 0.25*n_e * self.maxwellian_flux_speed(T_e, m_e)
 
     def gamma_neutral(self, n_neutral, T_neutral, m_neutral):
         return n_neutral*np.sqrt(8*e*T_neutral/(pi*N_A*m_neutral))/4
