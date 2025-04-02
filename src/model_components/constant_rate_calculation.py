86c411fc56be916115e331da381cea60ef672091
verdier audrey <solene.charlotte@gmail.com>
2025-04-02 16:36:27 +0200

    excitation OK

--- src/model_components/constant_rate_calculation.py
+++ src/model_components/constant_rate_calculation.py
@@ -4,15 +4,17 @@ from scipy.integrate import trapezoid, solve_ivp, odeint
 
 #Local modules
 from src.model_components.util import load_csv, load_cross_section
+#from util import load_csv, load_cross_section
 #from auxiliary_funcs import pressure, maxwellian_flux_speed, u_B, A_eff, A_eff_1, SIGMA_I, R_ind, h_L
 from src.model_components.specie import Specie, Species
+#from specie import Specie, Species
 
 def rate_constant(T_e, E, cs, m):
     """Calculates a reaction rate constant """
-    T = T_e * k / e
+    #T = T_e * k / e
     v = np.sqrt(2 * E * e / m)  # electrons speed
-    a = (m / (2 * np.pi * e * T))**(3/2) * 4 * np.pi
-    f = cs * v**3 * np.exp(- m * v**2 / (2 * e * T)) 
+    a = (m / (2 * np.pi * e * T_e))**(3/2) * 4 * np.pi
+    f = cs * v**3 * np.exp(- m * v**2 / (2 * e * T_e)) 
     k_rate = trapezoid(a*f, x=v)
     return k_rate
 
@@ -22,7 +24,6 @@ def get_K_func(species,specie:str,reaction:str):
         reaction: string, the type of reaction (ion_N,exc1_O,...)"""
     def get_K(state):
         T_e = state[species.nb]
-        n_e=state[0]
         e_r,cs_r=load_cross_section(".\\cross_sections\\"+specie+'\\'+reaction+'.csv')
         k_rate=rate_constant(T_e,e_r,cs_r,m_e) #si on considère que T_e en première approx
         return k_rate 
@@ -30,13 +31,14 @@ def get_K_func(species,specie:str,reaction:str):
 
 
 if __name__ == "__main__":
-    species_list = Species([Specie("e", 9.1e-31, 0),Specie("Xe0", 10.57e-27, 0), Specie("Xe+", 10.57e-27, 0)])
-    state = np.array([10^18,3*10^19,3*10^19,40000,500])
-    T_e = state[3]*k/e
+    #species_list = Species([Specie("e", 9.1e-31, 0),Specie("Xe0", 10.57e-27, 0), Specie("Xe+", 10.57e-27, 0)])
+    species_list = Species([Specie("e", 9.1e-31, -e, 0, 3/2), Specie("Xe", 2.18e-25, 0, 1, 3/2), Specie("Xe+", 2.18e-25, e, 1, 3/2)])
+    state = np.array([10^18,3*10^19,3*10^19,3,0.03])
+    T_e = state[3]
     #print(state[species_list.nb])
 
     get_K_ion=get_K_func(species_list, "Xe", "Ionization_Xe")
-    get_K_exc=get_K_func(species_list, "Xe", "Excitation1_Xe")
+    get_K_exc=get_K_func(species_list, "Xe", "exc_Xe")
     get_K_el=get_K_func(species_list, "Xe", "Elastic_Xe")
 
     k_rate_ion=get_K_ion(state)
@@ -48,8 +50,8 @@ if __name__ == "__main__":
     print("K_exc_exp="+str(k_rate_exc))
 
     #Threshold energies in eV
-    E_exc=11.6 #selon les valeurs des données du github: 8.32 eV
-    E_iz=12.13
+    E_exc=11.6  #selon les valeurs des données du github: 8.32 eV
+    E_iz=12.13 
 
     #Expressions in Chabert
     K_el= 1e-13
--- src/reaction_sets/Reaction_set_Xe_test1.py
+++ src/reaction_sets/Reaction_set_Xe_test1.py
@@ -1,4 +1,5 @@
 from scipy.constants import pi, e, k, epsilon_0 as eps_0, c, m_e
+import numpy as np
 
 from src.model_components.reactions.excitation_reaction import Excitation
 from src.model_components.reactions.ionisation_reaction import Ionisation
@@ -15,8 +16,16 @@ def get_species_and_reactions(chamber):
                     
     species = Species([Specie("e", m_e, -e, 0, 3/2), Specie("Xe", 2.18e-25, 0, 1, 3/2), Specie("Xe+", 2.18e-25, e, 1, 3/2)])
 
+    def Kexc(state):
+        T=state[species.nb]
+        Eexc=11.6
+        return 1.2921e-13 * np.exp(-Eexc/T)
+
+
     ### Excitation
-    #exc_Xe = Excitation(species, "Xe", get_K_func(species, "Xe", "exc_Xe"), 11.6, chamber) 
+    exc_Xe = Excitation(species, "Xe", get_K_func(species, "Xe", "exc_Xe"), 11.6, chamber) 
+    #get_K_func(species, "Xe", "exc_Xe")
+    #lambda T: Kexc(T)
 
     ### Elastic Collision : OK
     ##ela_elec_Xe = ElasticCollisionWithElectron(species, "Xe", lambda T : 1e-13, 0, chamber) # get_K_func(species, "Xe", "ela_elec_Xe")
@@ -25,10 +34,24 @@ def get_species_and_reactions(chamber):
     #src_Xe = GasInjection(species, [0.0, 1.2e19, 0], 0.03, chamber) 
 
     ### Sortie de Xe à travers les grilles
-    out_Xe = FluxToWallsAndThroughGrids(species, "Xe", get_K_func(species, "Xe", "out_Xe"), 0, chamber) 
+    ##out_Xe = FluxToWallsAndThroughGrids(species, "Xe", get_K_func(species, "Xe", "out_Xe"), 0, chamber) 
+
+    def Kiz(state):
+        T_e=state[species.nb]
+        E_iz=12.127
+        K_iz_1 = 6.73e-15 * (T_e)**0.5 * (3.97+0.643*T_e - 0.0368 * T_e**2) * np.exp(- E_iz/T_e)
+        K_iz_2 = 6.73e-15 * (T_e)**0.5 * (-0.0001031*T_e**2 + 6.386 * np.exp(- E_iz/T_e))
+        return 0.5 * (K_iz_1 + K_iz_2)
+
+    ### Ionisation
+    #ion_Xe =  
+    #ion_Xe = Ionisation(species, "Xe", "Xe+", get_K_func(species, "Xe", "Ionization_Xe"), 12.127, chamber) 
+    #get_K_func(species, "Xe", "Ionization_Xe")
+    #lambda T: 2.2384710835071163e-15
+    #lambda T: Kiz(T)
 
     # Reaction list
-    reaction_list = [out_Xe] #[exc_Xe, src_Xe] #[exc_Xe, src_Xe, out_Xe]
+    reaction_list = [exc_Xe] #[exc_Xe, src_Xe] #[exc_Xe, src_Xe, out_Xe]
 
     #electron_heating = ElectronHeatingConstantAbsorbedPower(species, 1e3, chamber) 
     electron_heating = ElectronHeatingConstantAbsorbedPower(species, 0, chamber)
