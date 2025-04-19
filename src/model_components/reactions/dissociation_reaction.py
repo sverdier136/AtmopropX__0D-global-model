8c3a1846f6637c9018e0d0e8d1c695d80b20b566
Charlélie CARER <charlelie.carer@gmail.com>
2025-04-19 02:46:30 +0200

    Dissociation corrected : mistakes in formulas and code

--- src/model_components/reactions/dissociation_reaction.py
+++ src/model_components/reactions/dissociation_reaction.py
@@ -1,4 +1,4 @@
-from typing import override
+from typing import override, Callable
 import numpy as np
 from numpy.typing import NDArray
 from scipy.constants import m_e, e, pi, k as k_B, epsilon_0 as eps_0, mu_0   # k is k_B -> Boltzmann constant
@@ -14,42 +14,51 @@ class Dissociation(Reaction):
     In reactives, electron must be in first position and colliding_specie next.
     """
 
-    def __init__(self, species: Species, rate_constant, energy_threshold: float, stoechio_coeffs: list, chamber: Chamber):
+    def __init__(self, species: Species, diatomic_reactive: str, monoatomic_product: str, rate_constant: Callable, energy_threshold: float, monoatomic_energy_excess: float, chamber: Chamber):
         """ Dissociation class
-        Parameters :
-        ------------- 
-        species : Instance of class Species, lists all species present 
-        colliding_specie : Name of specie that collides with an electron. Must be a string !
-        rate_constant : Function taking as argument state [n_e, n_N2, ..., n_N+, T_e, T_monoato, ..., T_diato]
-        energy_threshold : Energy threshold of electron so that reaction occurs
+        Parameters:
+        -------------
+        species : Species
+            Instance of class Species, lists all species present.
+        diatomic_reactive : str
+            Name of molecular reactive involved in the dissociation reaction.
+        monoatomic_product : str
+            Name of molecular product formed after the dissociation reaction. Should NOT contain the electron.
+        rate_constant : Callable
+            Function taking as argument the state [n_e, n_N2, ..., n_N+, T_e, T_monoato, ..., T_diato].
+        energy_threshold : float
+            Energy threshold of the electron so that the reaction occurs.
+        monoatomic_energy_excess : float
+            Energy excess of one monoatomic product after dissociation.
+        chamber : Chamber
+            Instance of the Chamber class containing chamber characteristics.
         """
-        super().__init__(species, [species.names[0], species.names], [species.names[0], species.names], chamber, stoechio_coeffs)
+        super().__init__(species, [species.names[0], diatomic_reactive], [species.names[0] + monoatomic_product], chamber)
         self.rate_constant = rate_constant
         self.energy_threshold = energy_threshold
+        self.monoatomic_energy_excess = monoatomic_energy_excess
 
     @override
     def density_change_rate(self, state):
         rate = np.zeros(self.species.nb)
         K = self.rate_constant(state)
-        sp_diato = self.reactives[1]
-        rate[sp_diato.index] -=  K * np.prod(state[self.reactives_indices])
-        for sp in self.species.species[1:]:
-            rate[sp.index] += 2 * K * np.prod(state[self.reactives_indices])
-        return np.zeros(self.species.nb)
+        sp_diato, sp_monoato = self.reactives[1], self.products[1]
+
+        reaction_speed = K * np.prod(state[self.reactives_indices])
+        rate[sp_diato.index] -=  reaction_speed
+        rate[sp_monoato.index] += 2 * reaction_speed
+
+        return rate
     
     @override
-    def energy_change_rate(self, state):
+    def energy_change_rate(self, state):  # TODO : Rajouter les termes de perte d'énergie dus a la baisse de densité de l'espèce diatomique
         rate = np.zeros(3)
 
         K = self.rate_constant(state)
         reac_speed = K * np.prod(state[self.reactives_indices])
         
-        rate[0] =  reac_speed * self.energy_threshold
+        rate[0] = - reac_speed * self.energy_threshold
         
-        Delta_E = 0.5 #d'après Esteves
-
-        for sp in self.species.species:
-            if sp.nb_atoms == 1:
-                rate[sp.nb_atoms] += reac_speed * 2 * Delta_E 
+        rate[1] = 2 * self.monoatomic_energy_excess * reac_speed 
         
         return rate
